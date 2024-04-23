c2049822be3b8e6a32492d21e4fe8f0a7232e219
unknown <hanadi.hosri@gmail.com>
2024-04-23 20:47:16 +0300

    fixing mistakes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './styles/utilities.css';
 import './styles/colors.css';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
     <div className="app">
-      
+      <Navbar />
     </div>
   );
 }
