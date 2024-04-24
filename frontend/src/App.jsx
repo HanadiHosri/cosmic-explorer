5fcf0fa8342a47a94714f97a2ddb974addd176ba
unknown <hanadi.hosri@gmail.com>
2024-04-24 16:30:37 +0300

    adding routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,18 @@ import './styles/utilities.css';
 import './styles/colors.css';
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home/index.jsx';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './pages/Login/index.jsx';
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
