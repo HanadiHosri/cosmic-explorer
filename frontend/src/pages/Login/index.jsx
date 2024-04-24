7fe655b6711e64822042e6e42df04d024910c3a8
unknown <hanadi.hosri@gmail.com>
2024-04-24 17:21:06 +0300

    adding login page

---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,33 @@
+import Navbar from "../../components/Navbar";
+import image from "../../assets/auth.jpg";
+import "./style.css";
+
+const Login = () => {
+    return (
+        <div className="page flex column">
+            <Navbar />
+            <div className="page-container flex column center">
+                <img className="img" src={image} />
+                <div className="form-container grey-bg rounded flex column center">
+                    <h1 className="white-text">Login</h1>
+                    <div className="flex column gap">
+                        <label className="white-text">Username</label>
+                        <input className="rounded input-field" type="text" placeholder="JohnDoe" />
+                    </div>
+                    <div className="flex column gap">
+                        <label className="white-text">Password</label>
+                        <input className="rounded input-field" type="password" placeholder="*********" />
+                    </div>
+                    <div>
+                        <p className="grey-text">Dont have an account? <a className="primary-text underline">Sign up</a> now</p>
+                    </div>
+                    <div>
+                        <button className="btn">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
